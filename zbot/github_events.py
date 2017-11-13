ee0c27a87ca5d75b0a9b7dfa27e803a880ccc08b
Leo Z <lzimann@users.noreply.github.com>
2017-11-13 11:32:52 -0200

    Everything now uses spaces instead of tab

--- zbot/github_events.py
+++ zbot/github_events.py
@@ -2,123 +2,123 @@ import json
 
 #bold = ""
 class EventHandler:
-	events_dict = {
-		'pull_request' : '_pull_request_event',
-		'push' : '_push_event',
-		'issues' : '_issue_event'
-	}
-	def __init__(self, event_type, payload, event_dict):
-		self.event_type = event_type
-		self.payload = json.loads(payload)
-		self.this_event_dict = event_dict
-		self.event_msg = self._check_event()
-		
-	def get_message(self):
-		return self.event_msg
-		
-	def _get_repo_name(self):
-		return "6[{repo_name}] ".format(repo_name = self.payload.get('repository').get('name'))
-	
-	#Returns the action color for issues/prs.
-	def _get_action_color(self, action, merging = False):
-		if action == 'opened' or action == 'reopened':
-			return '3' #Green
-		elif action == 'closed':
-			if merging:
-				return '6' #Purple
-			else:
-				return '4' #Red
-	
-	def _check_event(self):
-		try:
-			if self.event_type not in self.events_dict:
-				print("Error: {} is not a valid event".format(self.event_type))
-				raise KeyError
-			event = self.events_dict[self.event_type]
-			message = None
-			event_msg = getattr(self, event)()
-			if event_msg:
-				message = self._get_repo_name() + event_msg
-			return {'channels' : self.this_event_dict.get('channels'), 'message' : message}
-		except KeyError:
-			print("Invalid or not supported hook event:", self.event_type)
-			return {'message' : None}
+    events_dict = {
+        'pull_request' : '_pull_request_event',
+        'push' : '_push_event',
+        'issues' : '_issue_event'
+    }
+    def __init__(self, event_type, payload, event_dict):
+        self.event_type = event_type
+        self.payload = json.loads(payload)
+        self.this_event_dict = event_dict
+        self.event_msg = self._check_event()
 
-	def _pull_request_event(self):
-		pr_action = self.payload.get('action')
-		if pr_action not in self.this_event_dict.get('actions'):
-			return None
-		pr_obj = self.payload.get('pull_request')
-		merging = pr_obj.get('merged')
-		pr_action_color = self._get_action_color(pr_action, merging)
-		if pr_action == 'closed' and merging:
-			pr_action = 'merged'
-		pr_title = pr_obj.get('title')
-		pr_number = pr_obj.get('number')
-		pr_author = pr_obj.get('user').get('login')
-		if pr_author == self.payload.get('sender').get('login'):
-			pr_author = ""
-		else:
-			pr_author = " by {}".format(pr_author)
-		
-		to_branch = pr_obj.get('base').get('ref')
-		from_branch = pr_obj.get('head').get('ref')
-		
-		#the message itself
-		msg = "Pull Request "
-		msg += "{action_color}{action} ".format(action_color = pr_action_color, action = pr_action)
-		msg += "by {}: ".format(self.payload.get('sender').get('login'))
-		msg += "{title} (#{number}){author} ".format(title = pr_title, number = pr_number, author = pr_author)
-		msg += "({}...{}) ".format(to_branch, from_branch)
-		msg += pr_obj.get('html_url')
-		return msg
-		
-	def _push_event(self):
-		sender = self.payload.get('sender').get('login')
-		branch = self.payload.get('ref').replace('refs/heads/', '')
-		diff = self.payload.get('compare')
-		size = len(self.payload.get('commits'))
+    def get_message(self):
+        return self.event_msg
 
-		if self.payload.get('deleted'):
-			return "{} 4deleted {}.".format(sender, branch)
-		elif self.payload.get('created'):
-			return "{} 3created {}. {}".format(sender, branch, diff)
-	
-		if not size:
-			return None
-		
-		
-		#the message
-		msg = "Push: "
-		msg += "{} 6{}pushed {} commit{} ".format(sender, '4force-' if self.payload.get('forced') else '', size, '' if size == 1 else 's')
-		msg += "to {}. ".format(branch)
-		msg += diff
-		return msg
-	
-	def _issue_event(self):
-		issue_action = self.payload.get('action')
-		if issue_action not in self.this_event_dict.get('actions'):
-			return None
-		issue_obj = self.payload.get('issue')
-		issue_action_color = self._get_action_color(issue_action)
-		issue_title = issue_obj.get('title')
-		issue_number = issue_obj.get('number')
-		
-		#the message itself
-		msg = "Issue "
-		msg += "{action_color}{action} ".format(action_color = issue_action_color, action = issue_action)
-		msg += "by {}: ".format(self.payload.get('sender').get('login'))
-		msg += "{title} (#{number}). ".format(title = issue_title, number = issue_number)
-		msg += issue_obj.get('html_url')
-		return msg
+    def _get_repo_name(self):
+        return "6[{repo_name}] ".format(repo_name = self.payload.get('repository').get('name'))
+
+    #Returns the action color for issues/prs.
+    def _get_action_color(self, action, merging = False):
+        if action == 'opened' or action == 'reopened':
+            return '3' #Green
+        elif action == 'closed':
+            if merging:
+                return '6' #Purple
+            else:
+                return '4' #Red
+
+    def _check_event(self):
+        try:
+            if self.event_type not in self.events_dict:
+                print("Error: {} is not a valid event".format(self.event_type))
+                raise KeyError
+            event = self.events_dict[self.event_type]
+            message = None
+            event_msg = getattr(self, event)()
+            if event_msg:
+                message = self._get_repo_name() + event_msg
+            return {'channels' : self.this_event_dict.get('channels'), 'message' : message}
+        except KeyError:
+            print("Invalid or not supported hook event:", self.event_type)
+            return {'message' : None}
+
+    def _pull_request_event(self):
+        pr_action = self.payload.get('action')
+        if pr_action not in self.this_event_dict.get('actions'):
+            return None
+        pr_obj = self.payload.get('pull_request')
+        merging = pr_obj.get('merged')
+        pr_action_color = self._get_action_color(pr_action, merging)
+        if pr_action == 'closed' and merging:
+            pr_action = 'merged'
+        pr_title = pr_obj.get('title')
+        pr_number = pr_obj.get('number')
+        pr_author = pr_obj.get('user').get('login')
+        if pr_author == self.payload.get('sender').get('login'):
+            pr_author = ""
+        else:
+            pr_author = " by {}".format(pr_author)
+
+        to_branch = pr_obj.get('base').get('ref')
+        from_branch = pr_obj.get('head').get('ref')
+
+        #the message itself
+        msg = "Pull Request "
+        msg += "{action_color}{action} ".format(action_color = pr_action_color, action = pr_action)
+        msg += "by {}: ".format(self.payload.get('sender').get('login'))
+        msg += "{title} (#{number}){author} ".format(title = pr_title, number = pr_number, author = pr_author)
+        msg += "({}...{}) ".format(to_branch, from_branch)
+        msg += pr_obj.get('html_url')
+        return msg
+
+    def _push_event(self):
+        sender = self.payload.get('sender').get('login')
+        branch = self.payload.get('ref').replace('refs/heads/', '')
+        diff = self.payload.get('compare')
+        size = len(self.payload.get('commits'))
+
+        if self.payload.get('deleted'):
+            return "{} 4deleted {}.".format(sender, branch)
+        elif self.payload.get('created'):
+            return "{} 3created {}. {}".format(sender, branch, diff)
+
+        if not size:
+            return None
+
+
+        #the message
+        msg = "Push: "
+        msg += "{} 6{}pushed {} commit{} ".format(sender, '4force-' if self.payload.get('forced') else '', size, '' if size == 1 else 's')
+        msg += "to {}. ".format(branch)
+        msg += diff
+        return msg
+
+    def _issue_event(self):
+        issue_action = self.payload.get('action')
+        if issue_action not in self.this_event_dict.get('actions'):
+            return None
+        issue_obj = self.payload.get('issue')
+        issue_action_color = self._get_action_color(issue_action)
+        issue_title = issue_obj.get('title')
+        issue_number = issue_obj.get('number')
+
+        #the message itself
+        msg = "Issue "
+        msg += "{action_color}{action} ".format(action_color = issue_action_color, action = issue_action)
+        msg += "by {}: ".format(self.payload.get('sender').get('login'))
+        msg += "{title} (#{number}). ".format(title = issue_title, number = issue_number)
+        msg += issue_obj.get('html_url')
+        return msg
 
 class EventHandlerFactory:
-	def __init__(self, config):
-		self.events_dict = config.get('github').get('events')
-	
-	def new_event(self, event_type, payload):
-		if event_type not in self.events_dict:
-			return {'message' : None}
-		return EventHandler(event_type, payload, self.events_dict[event_type]).get_message()
-		
-		
+    def __init__(self, config):
+        self.events_dict = config.get('github').get('events')
+
+    def new_event(self, event_type, payload):
+        if event_type not in self.events_dict:
+            return {'message' : None}
+        return EventHandler(event_type, payload, self.events_dict[event_type]).get_message()
+
+
--- zbot/requests.py
+++ zbot/requests.py
@@ -4,85 +4,85 @@ import os
 import re
 from fuzzywuzzy import process, fuzz
 """
-	This will make requests using the GitHub api.
+    This will make requests using the GitHub api.
 """
 class APIRequests:
-	github_api_url = 'https://api.github.com/repos'
-	github_frontend_url = 'https://github.com'
-	file_pattern = re.compile('.*\/(.*)')
-	def __init__(self, config):
-		self.config = config
-		self.repo = self.config.get('name')
-		self.owner = self.config.get('owner')
-		self.master_tree_url = '{gau}/{o}/{r}/git/trees/master'.format(gau = self.github_api_url, o = self.owner, r = self.repo)
-		self.update_tree()	
-	"""
-		Updates the current tree, used as a hook for the bot
-	"""
-	def update_tree(self, force = False):
-		self.current_tree = self.get_repo_tree()
-		self.current_paths = []
-		for file in self.current_tree.get('tree'):
-			if file.get('type') == 'blob':
-				self.current_paths.append(file['path'])
-	
-	"""
-		Returns a URL to a commit if it exists
-	"""
-	def get_commit_url(self, sha):
-		api_url = '{gau}/{o}/{r}/commits/{commit}'.format(gau = self.github_api_url, o = self.owner, r = self.repo, commit = sha)
-		r = requests.get(api_url)
-		if r.status_code == 200:	#will 404 if it doesn't exist
-			frontend_url = '{feu}/{o}/{r}/commit/{commit}'.format(feu = self.github_frontend_url, o = self.owner, r = self.repo, commit = sha)
-			return frontend_url
+    github_api_url = 'https://api.github.com/repos'
+    github_frontend_url = 'https://github.com'
+    file_pattern = re.compile('.*\/(.*)')
+    def __init__(self, config):
+        self.config = config
+        self.repo = self.config.get('name')
+        self.owner = self.config.get('owner')
+        self.master_tree_url = '{gau}/{o}/{r}/git/trees/master'.format(gau = self.github_api_url, o = self.owner, r = self.repo)
+        self.update_tree()
+    """
+        Updates the current tree, used as a hook for the bot
+    """
+    def update_tree(self, force = False):
+        self.current_tree = self.get_repo_tree()
+        self.current_paths = []
+        for file in self.current_tree.get('tree'):
+            if file.get('type') == 'blob':
+                self.current_paths.append(file['path'])
 
-	"""
-		Returns a json tree of the default repo
-	"""
-	def get_repo_tree(self, force = False):
-		path = os.path.abspath('tree.json')
-		tree = {}
-		try:
-			f = open(path, "r+")
-			tree = json.load(f)
-		except:
-			f = open(path, "w+")
-		if tree.get('sha') != requests.get(self.master_tree_url).json().get('sha') or force:
-			f.seek(0)
-			f.truncate()
-			tree = requests.get(self.master_tree_url, params = {'recursive' : '1'}).json()
-			json.dump(tree, f)
-		return tree
-	
-	"""
-		Returns the current tree SHA
-	"""
-	def get_tree_sha(self):
-		return self.current_tree.get('sha')
-	
-	"""
-		Searches the tree for a given path
-	"""
-	def get_file_url(self, file_string, line):
-		matching_paths = []
-		file_to_search = file_string
-		user_match = self.file_pattern.search(file_string)
-		if user_match:
-			file_to_search = user_match.group(1)
-		for path in self.current_paths:
-			current_path = path
-			path_match = self.file_pattern.search(path)
-			if path_match:
-				current_path = path_match.group(1)
-			if fuzz.token_sort_ratio(file_to_search, current_path) >= 75:
-				matching_paths.append(path)
-		
-		result = process.extractOne(file_string, matching_paths, scorer = fuzz.token_set_ratio)
-		if result:
-			return "https://github.com/{own}/{repo}/blob/master/{p}{l}".format(own = self.owner, repo = self.repo, p = result[0], l = line if line else '')
-		return None
-	def get_pr_info(self, pr_number):
-		req = requests.get("{g}/{o}/{r}/issues/{n}".format(g = self.github_api_url, o = self.owner, r = self.repo, n = pr_number))
-		if req.status_code == 200:
-			return req.json()
-		return None
+    """
+        Returns a URL to a commit if it exists
+    """
+    def get_commit_url(self, sha):
+        api_url = '{gau}/{o}/{r}/commits/{commit}'.format(gau = self.github_api_url, o = self.owner, r = self.repo, commit = sha)
+        r = requests.get(api_url)
+        if r.status_code == 200:	#will 404 if it doesn't exist
+            frontend_url = '{feu}/{o}/{r}/commit/{commit}'.format(feu = self.github_frontend_url, o = self.owner, r = self.repo, commit = sha)
+            return frontend_url
+
+    """
+        Returns a json tree of the default repo
+    """
+    def get_repo_tree(self, force = False):
+        path = os.path.abspath('tree.json')
+        tree = {}
+        try:
+            f = open(path, "r+")
+            tree = json.load(f)
+        except:
+            f = open(path, "w+")
+        if tree.get('sha') != requests.get(self.master_tree_url).json().get('sha') or force:
+            f.seek(0)
+            f.truncate()
+            tree = requests.get(self.master_tree_url, params = {'recursive' : '1'}).json()
+            json.dump(tree, f)
+        return tree
+
+    """
+        Returns the current tree SHA
+    """
+    def get_tree_sha(self):
+        return self.current_tree.get('sha')
+
+    """
+        Searches the tree for a given path
+    """
+    def get_file_url(self, file_string, line):
+        matching_paths = []
+        file_to_search = file_string
+        user_match = self.file_pattern.search(file_string)
+        if user_match:
+            file_to_search = user_match.group(1)
+        for path in self.current_paths:
+            current_path = path
+            path_match = self.file_pattern.search(path)
+            if path_match:
+                current_path = path_match.group(1)
+            if fuzz.token_sort_ratio(file_to_search, current_path) >= 75:
+                matching_paths.append(path)
+
+        result = process.extractOne(file_string, matching_paths, scorer = fuzz.token_set_ratio)
+        if result:
+            return "https://github.com/{own}/{repo}/blob/master/{p}{l}".format(own = self.owner, repo = self.repo, p = result[0], l = line if line else '')
+        return None
+    def get_pr_info(self, pr_number):
+        req = requests.get("{g}/{o}/{r}/issues/{n}".format(g = self.github_api_url, o = self.owner, r = self.repo, n = pr_number))
+        if req.status_code == 200:
+            return req.json()
+        return None
