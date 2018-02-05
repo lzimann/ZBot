ee17a9cd7609ce161fa787a4d9123e72fc6d4337
Leo Z <lzimann@users.noreply.github.com>
2018-02-05 17:15:57 -0200

    Switches kwargs check order

--- zbot/bot.py
+++ zbot/bot.py
@@ -125,7 +125,7 @@ class ZBot(irc.IRCClient):
         """The decorator allows commands that require arguments to display the help if no arg is passed"""
         @functools.wraps(func)
         def check_arg(self, *args, **kwargs):
-            if len(args[2]) == 1 and kwargs.get('regex_used') is None:
+            if kwargs.get('regex_used') is None and len(args[2]) == 1:
                 args[2].insert(0, "shelp")
                 return self._help(*args, **kwargs)
             return func(self, *args, **kwargs)
